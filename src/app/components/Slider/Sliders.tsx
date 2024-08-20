2a6e59f5870ac763dbaf67a557107d0488d74e26
lucasbordignon0 <l.bordignon0@gmail.com>
2024-08-20 17:33:04 +0200

    push

---
+++ src/app/components/Slider/Sliders.tsx
@@ -0,0 +1,7 @@
+export function Sliders() {
+    return (
+        <div className="p-4 rounded-2xl bg-[#202020]">
+            <h1>Sliders</h1>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import DynamicSettings from "./components/FloatingNavBar/DynamicSettings";
 import { NavBar } from "./components/FloatingNavBar/NavBar";
+import { Sliders } from "./components/Slider/Sliders";
 
 export default function Home() {
   return (
-      <div className="flex w-full h-screen justify-center items-center">
-        <NavBar />
-      </div>
+    <div className="flex flex-col gap-6 w-full h-screen justify-center items-center">
+      <NavBar />
+    </div>
   );
 }
